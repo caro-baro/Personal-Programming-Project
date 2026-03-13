f110af23a415403b09422fc907a8b111e2abedeb
carobaro <cbararduc28@connect.kellettschool.com>
2026-03-13 07:07:44 +0000

    Mar 13, 2026, 3:07 PM

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 ## Personal Programming Project - Carolina 
+fund = 1000
+deck = ['U+1F0A1']
 
-# hello
+def hello_to_user():
+    print("Hello, we are playing\nBlackjack")
+    
+
+
+def place_bets(bet):
+    print(f"You have: ${fund}")
+    bet = int(input("Place your bets:\n"))
+    return bet
+
+
+
+def show_cards():
+    print()
+
+
+
+def ask_choice():
+    choice = input("Hit or Stand?")
+
+
+
+
+if __name__ == "__main__":
+    hello_to_user()
+    place_bets(fund)
+
